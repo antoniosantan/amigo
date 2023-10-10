6a9a480a3b9d96b902335809c8815dd51df12685
Vitor Santa <v171822an@gmail.com>
2023-10-10 16:55:12 -0300

    primeiro_commit

---
+++ contatos/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Amigo
+
+class AmigoSerializer(ModelSerializer):
+    class Meta:
+        model = Amigo 
+        fields = "__all__"
